2c6db28d0f2ba6bbac17cad4bc9a3a5c8adfb28e
g01198628y <g01198628y@gmail.com>
2018-04-04 12:41:53 +0800

    debug,add caching reference

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -14,7 +14,8 @@ namespace Vidly
                         "~/Scripts/bootbox.js",
                         "~/Scripts/respond.js",
                         "~/Scripts/datatables/jquery.datatables.js",
-                        "~/Scripts/datatables/datatables.bootstrap.js"));
+                        "~/Scripts/datatables/datatables.bootstrap.js",
+                        "~/Scripts/typeahead.bundle.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -69,7 +69,7 @@ namespace Vidly.Controllers
         {
             if (MemoryCache.Default["Genres"] == null)
             {
-                MemoryCache.Default["Genres"] = _context.Genres.Tolist();
+                MemoryCache.Default["Genres"] = _context.Genres.ToList();
             }
             var genres = MemoryCache.Default["Genres"] as IEnumerable<Genre>;
             return View();
